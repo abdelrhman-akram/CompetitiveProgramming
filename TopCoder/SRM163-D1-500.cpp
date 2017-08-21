8aa72a65ba52b72c2ae582820a559325504b6d98
abdelrhman-akram <30937895+abdelrhman-akram@users.noreply.github.com>
2017-08-22 00:21:49 +0200

    Update SRM163-D1-500.cpp

--- TopCoder/SRM163-D1-500.cpp
+++ TopCoder/SRM163-D1-500.cpp
@@ -1,62 +1,101 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-class OneArmedBandit
+
+class Pool
 {
 public:
-    double progressiveJackpot(vector <string> wheels, string jackpotLine, vector <string> payoffTable)
+    int rackMoves(vector <int> triangle)
     {
-        vector<map<char,double> >vec;
-        vec.resize(wheels.size());
-        for (int i=0; i<wheels.size(); i++)
+        vector<int>vec1,vec2;
+        for (int i=1; i<=5; i++)
         {
-            map<char,double> temp;
-            for (int j=0; j<wheels[i].size(); j++)
+            for (int j=0; j<i; j++)
             {
-                temp[wheels[i][j]]++;
+                if (i==5&&j==4)
+                {
+                    vec1.push_back(1),vec2.push_back(0);
+                    break;
+                }
+                if (i%2==1)
+                {
+                    if (j%2==0)
+                        vec1.push_back(0),vec2.push_back(1);
+                    if (i==3&&j%2==1)
+                        vec1.push_back(8),vec2.push_back(8);
+                    else if (j%2==1)
+                        vec1.push_back(1),vec2.push_back(0);
+                }
+                else
+                {
+                    if (i==2)
+                        vec1.push_back(1),vec2.push_back(0);
+                    else if (j%2==1)
+                        vec1.push_back(0),vec2.push_back(1);
+                    else if (j%2==0)
+                        vec1.push_back(1),vec2.push_back(0);
+                }
             }
-            vec[i]=temp;
         }
-        double prop=1;
-        for (int i=0; i<jackpotLine.size(); i++)
+        for (int i=0; i<triangle.size(); i++)
         {
-            char x=jackpotLine[i];
-            if (x=='-')
-                continue;
-            if (vec[i][x]==0)
-                return -1;
-            prop*=vec[i][x]/(double)wheels[i].size();
+            if (triangle[i]<8)
+                triangle[i]=0;
+            else if (triangle[i]>8)
+                triangle[i]=1;
         }
-        double total=0;
-        for (int i=0; i<payoffTable.size(); i++)
+        int mi=1e9;
+        vector<int> vec3=triangle;
+        int c=0;
+        for (int i=0; i<triangle.size(); i++)
         {
-            double t=1.0,pay;
-            stringstream ss(payoffTable[i]);
-            string tem;
-            ss>>tem;
-            ss>>pay;
-            for (int j=0; j<tem.size(); j++)
+            if (triangle[i]!=vec2[i])
             {
-                char x=tem[j];
-                if (x=='-')
-                    continue;
-                t*=(vec[j][x]/(double)wheels[j].size());
-                if (t>=1)
-                    return 0;
+                for (int j=0; j<triangle.size(); j++)
+                {
+                    if (i==j)
+                        continue;
+                    if (triangle[j]!=vec2[j]&&triangle[j]==vec2[i]&&triangle[i]==vec2[j])
+                    {
+                        swap(triangle[j],triangle[i]);
+                        c++;
+                        break;
+                    }
+                }
             }
-            t*=pay;
-            total+=t;
         }
-        if (total>=1)
-            return 0;
-        total=1-total;
-        total=(double)total/prop;
-        return total;
+        if(triangle[4]!=8)
+            c+=2;
+        mi=min(mi,c);
+        triangle=vec3;
+        c=0;
+        for (int i=0; i<triangle.size(); i++)
+        {
+            if (triangle[i]!=vec1[i])
+            {
+                for (int j=0; j<triangle.size(); j++)
+                {
+                    if (i==j)
+                        continue;
+                    if (triangle[j]!=vec1[j]&&triangle[j]==vec1[i]&&triangle[i]==vec1[j])
+                    {
+                        swap(triangle[j],triangle[i]);
+                        c++;
+                        break;
+                    }
+                }
+            }
+        }
+        if (triangle[4]!=8)
+            c+=2;
+        mi=min(mi,c);
+        return mi;
     }
 };
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
+
     return 0;
 }
