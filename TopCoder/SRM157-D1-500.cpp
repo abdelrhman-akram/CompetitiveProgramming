a1ea5001b8d7c1328bad3e8e8904a00573c60015
abdelrhman-akram <30937895+abdelrhman-akram@users.noreply.github.com>
2017-08-22 01:14:23 +0200

    Update SRM157-D1-500.cpp

--- TopCoder/SRM157-D1-500.cpp
+++ TopCoder/SRM157-D1-500.cpp
@@ -1,40 +1,24 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-
 class Table
 {
 public:
     vector <string> layout(vector <string> tbl)
     {
         vector<string> mat;
-        int mc=0,mr=0;
-        for (int i=0;i<tbl.size();i++){
-                if (tbl[i].size()==0)
-                continue;
-                int c=0,r=0;
-            for (int j=1;j<tbl.size();j++){
-                c+=(tbl[i][j]-'0');
-                j+=2;
-                r=max(r,tbl[i][j]-'0');
-                j+=2;
-                j+=2;
-            }
-            mc=max(c,mc);
-            mr=max(mr,i+r);
-        }
-        mat.resize(60);
+        mat.resize(500);
         for (int i=0;i<mat.size();i++)
-            mat[i].resize(60);
+            mat[i].resize(500);
+            int row;
         for (int i=0; i<(int)tbl.size(); i++)
         {
-
             if (tbl[i].size()==0)
                 continue;
             for (int j=1; j<(int)tbl[i].size(); j++)
             {
                 int col;
-                for(int k=0;k<tbl[i].size();k++)
+                for(int k=0;k<500;k++)
                 {
                     if (mat[i][k]=='\0'){
                         col=k;
@@ -49,7 +33,6 @@ public:
                 j+=2;
                 o=tbl[i][j];
                 j+=2;
-
                 for (int k=i; k<i+r; k++)
                 {
                     for (int f=0; f<c; f++)
@@ -57,10 +40,7 @@ public:
                         mat[k][col+f]=o;
                     }
                 }
-
-                col+=c;
             }
-
         }
         vector<string>ret;
         for (int i=0;i<mat.size();i++){
@@ -75,3 +55,9 @@ public:
         return ret;
     }
 };
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+    return 0;
+}
